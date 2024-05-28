3cc7d0d729be40e082d7882ae8c7f35812590ca3
José Leonardo Zúñiga Hernández <114005429+Leonides2@users.noreply.github.com>
2024-05-28 16:51:23 -0600

    update

--- Services/Services/Email/EmailService.cs
+++ Services/Services/Email/EmailService.cs
@@ -9,17 +9,23 @@ namespace Services.Services.Email
 {
     public class EmailService : IEmailService
     {
-       // private readonly SmtpSettings _smtpSettings;
 
-        public EmailService(
-            //IOptions<SmtpSettings> smtpSettings
-            )
+        public EmailService()
         {
-            //_smtpSettings = smtpSettings.Value;
+            
         }
 
         public async Task SendEmailAsync(string Email, string subject, string msg, SmtpSettings settings)
         {
+            string body = "<!DOCTYPE html>\r\n<html>\r\n" +
+                "<head>\r\n  " +
+                "<link rel='stylesheet' type='text/css' media='screen' href='main.css'>\r\n" +
+                "</head>\r\n" +
+                "<body style=\"width: 100%;\">\r\n    " +
+                "<h2 style=\"font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;\">"+ subject +" </h2>\r\n    " +
+                "<h3 style=\"font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;\"> "+ msg +"</h3>\r\n\r\n    " +
+                "<p style=\"word-wrap: break-word; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;\"> Por favor no responda a este correo, es un correo generado automaticamente</p>\r\n" +
+                "</body>\r\n</html>";
             try
             {
                 var client = new SmtpClient(settings.Server, settings.Port)
@@ -33,7 +39,7 @@ namespace Services.Services.Email
                 {
                     From = new MailAddress(settings.Username!),
                     Subject = subject,
-                    Body = msg,
+                    Body = body,
                     IsBodyHtml = true
                 };
 
